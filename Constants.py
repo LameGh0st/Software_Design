b4e236c81c3c568ba937b02b531bf9a867c217dd
LameGh0st <cstafford@gm.slc.edu>
2022-03-22 12:10:20 -0400

    Added Constants File

---
+++ Constants.py
@@ -0,0 +1,16 @@
+from Board_Class import *
+
+#-----------------------------Constants---------------------------------------
+player_hidden_board = Board(10,10)
+player_guess_board = Board(10,10)
+bot_hidden_board = Board(10,10)
+bot_guess_board = Board(10,10)
+
+
+length = 500
+width = 500
+offset = 50
+tx = .5
+
+delta_width = ((width - 2 * offset) / bot_hidden_board.width) 
+delta_length = ((length - 2 * offset) / bot_hidden_board.length)
